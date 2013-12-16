5345a902eee7bdbdb4b479eb383f170f95d74aa6
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2013-12-17 00:58:00 +0200

    Added check function, renamed hash to crypt.

--- lib/resty/scrypt.lua
+++ lib/resty/scrypt.lua
@@ -8,19 +8,12 @@ int crypto_scrypt(const uint8_t * passwd, size_t passwdlen, const uint8_t * salt
 int calibrate(size_t maxmem, double maxmemfrac, double maxtime, uint64_t *n, uint32_t *r, uint32_t *p );
 ]]
 
-local sc = ffi.load("./scryptc.so")
+local scrypt = ffi.load("./scrypt.so")
 
 local function hex(str)
     return (str:gsub('.', function(c)
         return string.format("%02x", string.byte(c))
     end))
-end
-
-local function salt(options)
-    if (type(options.salt_size) ~= "number") then
-        options.salt_size = 8
-    end
-    return hex(random.bytes(options.salt_size))
 end
 
 local function calibrate(options)
@@ -36,13 +29,16 @@ local function calibrate(options)
     local n = ffi_new("uint64_t[1]", 0)
     local r = ffi_new("uint32_t[1]", 0)
     local p = ffi_new("uint32_t[1]", 0)
-    if (sc.calibrate(options.maxmem, options.memfrac, options.maxtime, n, r, p) == 0) then
+    if (scrypt.calibrate(options.maxmem, options.memfrac, options.maxtime, n, r, p) == 0) then
         return tonumber(n[0]), r[0], p[0]
     end
     return false
 end
 
-local function hash(options)
+local function crypt(options)
+    if (type(options) ~= "table") then
+        options = { secret = tostring(options) }
+    end
     if (type(options.key_len) ~= "number") then
         options.key_len  = 32
     end
@@ -53,22 +49,34 @@ local function hash(options)
         options.n, options.r, options.p = calibrate(options)
     end
     if (type(options.salt) ~= "string") then
-        options.salt = salt(options)
+        if (type(options.salt_size) ~= "number") then
+            options.salt_size = 8
+        end
+        options.salt = hex(random.bytes(options.salt_size))
     end
-
     local n = ffi_new("uint64_t[1]", options.n)
     local r = ffi_new("uint32_t[1]", options.r)
     local p = ffi_new("uint32_t[1]", options.p)
     local b = ffi_new("uint8_t[?]",  options.key_len)
-
-    if (sc.crypto_scrypt(options.secret, #options.secret, options.salt, #options.salt, n[0], r[0], p[0], b, options.key_len) == 0) then
+    if (scrypt.crypto_scrypt(options.secret, #options.secret, options.salt, #options.salt, n[0], r[0], p[0], b, options.key_len) == 0) then
         return string.format("%02x$%02x$%02x$", tonumber(n[0]), r[0], p[0]) .. options.salt .. "$" .. hex(ffi_str(b, options.key_len))
     end
-
     return false
 end
 
+local function check(secret, hash)
+    local options = {}
+    local n, r, p, salt = hash:match(("([^$]*)$"):rep(5))
+    options.secret = secret
+    options.salt = salt
+    options.n = tonumber(n, 16)
+    options.r = tonumber(r, 16)
+    options.p = tonumber(p, 16)
+    return crypt(options) == hash
+end
+
 return {
-    calibrate = calibrate,
-    hash      = hash
+    crypt     = crypt,
+    check     = check,
+    calibrate = calibrate
 }
