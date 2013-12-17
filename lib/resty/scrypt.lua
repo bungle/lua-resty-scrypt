71e9dc682b49fbe4fd84a1b064027fc69ae312ee
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2013-12-17 02:13:23 +0200

    More sane default, simplified api.

--- lib/resty/scrypt.lua
+++ lib/resty/scrypt.lua
@@ -5,7 +5,7 @@ local ffi_str = ffi.string
 
 ffi.cdef[[
 int crypto_scrypt(const uint8_t * passwd, size_t passwdlen, const uint8_t * salt, size_t saltlen, uint64_t N, uint32_t _r, uint32_t _p, uint8_t * buf, size_t buflen);
-int calibrate(size_t maxmem, double maxmemfrac, double maxtime, uint64_t *n, uint32_t *r, uint32_t *p );
+int calibrate(size_t maxmem, double maxmemfrac, double maxtime, uint64_t *n, uint32_t *r, uint32_t *p);
 ]]
 
 local scrypt = ffi.load("./scrypt.so")
@@ -16,50 +16,48 @@ local function hex(str)
     end))
 end
 
-local function calibrate(options)
-    if (type(options.maxmem) ~= "number") then
-        options.maxmem  = 1024 * 1024
-    end
-    if (type(options.memfrac) ~= "number") then
-        options.memfrac = 0.5
-    end
-    if (type(options.maxtime) ~= "number") then
-        options.maxtime = 0.2
-    end
-    local n = ffi_new("uint64_t[1]", 0)
-    local r = ffi_new("uint32_t[1]", 0)
-    local p = ffi_new("uint32_t[1]", 0)
-    if (scrypt.calibrate(options.maxmem, options.memfrac, options.maxtime, n, r, p) == 0) then
-        return tonumber(n[0]), r[0], p[0]
-    end
-    return false
-end
-
 local function crypt(options)
-    if (type(options) ~= "table") then
+    if type(options) ~= "table" then
         options = { secret = tostring(options) }
-    end
-    if (type(options.key_len) ~= "number") then
-        options.key_len  = 32
-    end
-    if (type(options.secret) ~= "string") then
+    elseif (type(options.secret) ~= "string") then
         options.secret = tostring(options.secret)
     end
-    if (type(options.n) ~= "number" or type(options.r) ~= "number" or type(options.p) ~= "number") then
-        options.n, options.r, options.p = calibrate(options)
-    end
-    if (type(options.salt) ~= "string") then
-        if (type(options.salt_size) ~= "number") then
-            options.salt_size = 8
+    if type(options.keysize) ~= "number" then
+        options.keysize  = 32
+    else
+        if options.keysize < 16 then
+            options.keysize = 16
+        elseif (options.keysize > 512) then
+            options.keysize = 512
         end
-        options.salt = hex(random.bytes(options.salt_size))
+    end
+    if type(options.n) ~= "number" then
+        options.n = 32768
+    end
+    if type(options.r) ~= "number" then
+        options.r = 8
+    end
+    if type(options.p) ~= "number" then
+        options.p = 1
+    end
+    if type(options.salt) ~= "string" then
+        if type(options.saltsize) ~= "number" then
+            options.saltsize = 8
+        else
+            if options.saltsize < 8 then
+                options.saltsize = 8
+            elseif (options.saltsize > 32) then
+                options.saltsize = 32
+            end
+        end
+        options.salt = hex(random.bytes(options.saltsize))
     end
     local n = ffi_new("uint64_t[1]", options.n)
     local r = ffi_new("uint32_t[1]", options.r)
     local p = ffi_new("uint32_t[1]", options.p)
-    local b = ffi_new("uint8_t[?]",  options.key_len)
-    if (scrypt.crypto_scrypt(options.secret, #options.secret, options.salt, #options.salt, n[0], r[0], p[0], b, options.key_len) == 0) then
-        return string.format("%02x$%02x$%02x$", tonumber(n[0]), r[0], p[0]) .. options.salt .. "$" .. hex(ffi_str(b, options.key_len))
+    local b = ffi_new("uint8_t[?]",  options.keysize)
+    if (scrypt.crypto_scrypt(options.secret, #options.secret, options.salt, #options.salt, n[0], r[0], p[0], b, options.keysize) == 0) then
+        return string.format("%02x$%02x$%02x$", tonumber(n[0]), r[0], p[0]) .. options.salt .. "$" .. hex(ffi_str(b, options.keysize))
     end
     return false
 end
@@ -75,8 +73,32 @@ local function check(secret, hash)
     return crypt(options) == hash
 end
 
+local function calibrate(maxmem, maxmemfrac, maxtime)
+    if type(maxmem) ~= "number" then
+        maxmem = 1048576
+    end
+    if type(maxmemfrac) ~= "number" then
+        maxmemfrac = 0.5
+    end
+    if type(maxtime) ~= "number" then
+        maxtime = 0.2
+    end
+    local n = ffi_new("uint64_t[1]", 0)
+    local r = ffi_new("uint32_t[1]", 0)
+    local p = ffi_new("uint32_t[1]", 0)
+    if (scrypt.calibrate(maxmem, maxmemfrac, maxtime, n, r, p) == 0) then
+        return tonumber(n[0]), r[0], p[0]
+    end
+    return false
+end
+
+local function memoryuse(n, r, p)
+    return 128 * r * p + 256 * r + 128 * r * n;
+end
+
 return {
     crypt     = crypt,
     check     = check,
-    calibrate = calibrate
+    calibrate = calibrate,
+    memoryuse = memoryuse
 }
